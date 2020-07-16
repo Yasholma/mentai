655df2df483a1cf67897ad1e1de65b95a419b999
Yahaya <yasholma1@gmail.com>
2020-07-16 14:22:22 +0100

    update index.html

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>MentAi Landing Page</title>
+    <!-- Favicon -->
+    <link
+      rel="shortcut icon"
+      href="./assets/img/favicon.ico"
+      type="image/x-icon"
+    />
+    <link rel="icon" href="./assets/img/favicon.ico" type="image/x-icon" />
     <!-- Bootstrap -->
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
     <!-- Font-awesome -->
