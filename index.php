4b068cda4757516aa40c7eed234451f72b51c4ff
Yahaya <yasholma1@gmail.com>
2020-07-17 09:12:16 +0100

    update video

--- index.php
+++ index.php
@@ -628,6 +628,22 @@
       </div>
       <!-- Board Ends -->
 
+      <!-- *** Video Section *** -->
+      <div class="video">
+        <div class="content-box">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-10 mx-auto">
+                <video width="100%" height="100%" controls>
+                  <source src="./assets/mentai.mp4" type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <!-- ** Footer ** -->
       <footer class="footer text-center">
         <p class="copyright">2020 &copy; Copyrights Reserved</p>
